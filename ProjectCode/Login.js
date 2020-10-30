8012b9e0aa2949e2562676d70aa8a0a91aa9081c
thde0834 <60409880+thde0834@users.noreply.github.com>
2020-10-30 16:29:29 -0600

    Updated Login.js functionality
    checkCredentials() should work:
    - email has to be @colorado.edu to proceed
    - email and password fields need to be filled to proceed
    To do:
    - check the database to check if the account exists.

--- ProjectCode/Login.js
+++ ProjectCode/Login.js
@@ -1,4 +1,3 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react'
 import {
   View,
@@ -6,27 +5,68 @@ import {
   TextInput,
   StyleSheet,
   TouchableOpacity,
-  Text
+  Text,
+  Alert
 } from 'react-native'
+import SignUp from "./SignUp";
+import MainFeed from "./MainFeed";
+import {auth} from "../utility/database"
 
-export default class SignUp extends React.Component {
+class LogInComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.navigate = props.navigate;
+  }
+
   state = {
     email: '', password: ''
+  };
+
+  checkCredentials(email, password){
+    // check if email includes @colorado.edu
+    if (!email.match(/\b[A-Za-z0-9._%+-]+@colorado.edu/g)){
+      this.displayErrorMessage("Please use your @colorado.edu email.");
+      return false;
+    }
+
+    // check if email isn't blank
+    if (email == ''){
+      this.displayErrorMessage("email plz");
+      return false;
+    }
+
+    // check password isn't blank
+    if (password === ''){
+      this.displayErrorMessage("Password plz");
+      return false;
+    }
+
+    return true;
   }
+
+  //displays error message on screen
+  displayErrorMessage(error){
+    Alert.alert(error);
+  }
+
   onChangeText = (key, val) => {
     this.setState({ [key]: val })
-  }
-  login = () => 
-  {
-    const { email, password } = this.state
-    alert("Successfully Logged In!")
-  }
+  };
 
-  navigate = () => 
-  {
+  login = () => {
+    const {email, password} = this.state;
+    // check passwords match, and not blank
+    if (this.checkCredentials(email, password) {
+      auth.signInWithEmailAndPassword(email, password).then(function (cred) {
+        Alert.alert("Successfully Signed Up!")
+      }).catch(function (error) {
+        this.displayErrorMessage("Something went wrong.");
+      });
 
-  }
- 
+    }
+
+  };
+
   render() {
     return (
       <View style={styles.container}>
@@ -45,16 +85,24 @@ export default class SignUp extends React.Component {
           placeholderTextColor='#ABABAB'
           onChangeText={val => this.onChangeText('password', val)}
         />
-        <TouchableOpacity onPress={this.login}>
+        <TouchableOpacity onPress={() => this.navigate.to(MainFeed)}>
           <Text style={styles.login}>Login</Text>
         </TouchableOpacity>
-        <TouchableOpacity onPress={this.navigate}>
+        <TouchableOpacity onPress={() => this.navigate.to(SignUp)}>
           <Text style={styles.signup}>Don't have an account? Sign Up</Text>
         </TouchableOpacity>
       </View>
     )
   }
 }
+
+
+export default function LogIn(navigate) {
+  return (
+      <LogInComponent navigate={navigate}/>
+  )
+}
+
 
 const styles = StyleSheet.create({
   input: {
